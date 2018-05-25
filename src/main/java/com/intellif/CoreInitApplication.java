924df0db7868268eabe17442c5bedddb8f391134
yinchong <yin.chong@intellif.com>
2018-05-25 13:31:21 +0800

    添加生成根据sql语句生成配置

--- src/main/java/com/intellif/CoreInitApplication.java
+++ src/main/java/com/intellif/CoreInitApplication.java
@@ -1,12 +1,17 @@
 package com.intellif;
 
+import com.intellif.aware.BeanRegister;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
+
+@EnableAutoConfiguration
 @SpringBootApplication
 public class CoreInitApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CoreInitApplication.class, args);
+		ApplicationContext context = SpringApplication.run(CoreInitApplication.class, args);
 	}
 }
--- src/main/java/com/intellif/aware/BeanAware.java
+++ src/main/java/com/intellif/aware/BeanAware.java
@@ -1,22 +1,46 @@
 package com.intellif.aware;
 
 import org.springframework.beans.BeansException;
-import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.config.BeanPostProcessor;
+import org.springframework.cglib.proxy.Enhancer;
+import org.springframework.cglib.proxy.MethodInterceptor;
+import org.springframework.cglib.proxy.MethodProxy;
 import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Component
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Method;
+
+//@Component
 public class BeanAware implements BeanPostProcessor {
 
     @Override
     public Object postProcessBeforeInitialization(Object bean, String beanName) throws BeansException {
-       // System.out.println(">>>>>>>>>>>>>>>>>>>>>>>>>>>bean对象初始化前调用");
+        System.out.println(">>>>>>>>>>>>>>>>>>>>>>>>>>>bean对象初始化前调用");
         return bean;
+
     }
 
     @Override
     public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
-        //System.out.println(">>>>>>>>>>>>>>>>>>>>>>>bean对象初始化后调用");
+        Class clazz = bean.getClass();
+       Annotation annotation =  clazz.getAnnotation(RestController.class);
+       if(annotation==null)
         return bean;
+
+        Enhancer enhancer = new Enhancer();
+        enhancer.setSuperclass(bean.getClass());
+        enhancer.setCallback(new MethodInterceptor() {
+            @Override
+            public Object intercept(Object o, Method method, Object[] objects, MethodProxy methodProxy) throws Throwable {
+              RequestMapping requestMapping =   method.getAnnotation(RequestMapping.class);
+                System.out.println(">>>>>>>>>>>>>>>方法调用前:"+method.getName()+" requestMapping:"+requestMapping.value()[0]);
+                Object result = methodProxy.invokeSuper(o,objects);
+                System.out.println(">>>>>>>>>>>>>>>>>>方法调用后");
+                return result;
+            }
+        });
+        return enhancer.create();
     }
 }
