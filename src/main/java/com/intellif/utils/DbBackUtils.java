cc5579a2c76a032fb3dc9de0ce4a77d6bd3da36f
yinchong <yin.chong@intellif.com>
2018-06-08 15:07:58 +0800

    添加备份数据工具类

---
+++ src/main/java/com/intellif/utils/DbBackUtils.java
@@ -0,0 +1,111 @@
+package com.intellif.utils;
+
+import java.io.*;
+
+/**
+ * MySQL数据库备份
+ */
+public class DbBackUtils {
+
+    /**
+     * 备份数据库
+     * @param ip 数据库ip地址
+     * @param port 端口
+     * @param userName 用户名
+     * @param password 密码
+     * @param savePath 保存路径
+     * @param fileName 文件名
+     * @param dataBaseName 库名
+     * @return
+     */
+    public static boolean dbBackup(String ip,int port,String userName,String password,String savePath,String fileName,String dataBaseName){
+        File saveFile = new File(savePath);
+        if(!saveFile.exists()){
+            saveFile.mkdirs();
+        }
+        if(!savePath.endsWith(File.separator)){
+            savePath = savePath+File.separator;
+        }
+        try {
+           OutputStream os = new FileOutputStream(savePath + fileName);
+            Process process = Runtime.getRuntime().exec("mysqldump -h " + ip + " -P "
+                    + port + " -u " + userName + " -p" + password + " --set-charset=UTF8 " + dataBaseName);
+           InputStream in = process.getInputStream();
+           FileUtils.saveText(in,os);
+           if(process.waitFor() == 0){//0 表示线程正常终止。
+               return true;
+           }
+        }catch (Exception e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+
+    /**
+     * 执行sql文件
+     *
+     * @param savePath
+     * @throws Exception
+     */
+    public static boolean dbRecover(String dbUser, String dbPass, String dbHost,
+                                 String dbPort, String dbName, String savePath) throws Exception {
+        try {
+            // 获取操作数据库的相关属性
+            Runtime runtime = Runtime.getRuntime();
+            Process process = runtime.exec("mysql -h" + dbHost + " -P " + dbPort
+                    + " -u " + dbUser + " -p" + dbPass
+                    + " --default-character-set=utf8 " + dbName);
+            OutputStream os = process.getOutputStream();
+            InputStream in = new FileInputStream(savePath);
+            FileUtils.saveText(in,os);
+            if(process.waitFor() == 0){//0 表示线程正常终止。
+                return true;
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return false;
+    }
+
+    /**
+     * 备份数据库
+     * @param ip 数据库ip地址
+     * @param port 端口
+     * @param userName 用户名
+     * @param password 密码
+     * @param savePath 保存路径
+     * @param fileName 文件名
+     * @param dataBaseName 库名
+     * @return
+     */
+    public static boolean tableBackup(String ip,int port,String userName,String password,String savePath,String fileName,String dataBaseName,String tableName){
+        File saveFile = new File(savePath);
+        if(!saveFile.exists()){
+            saveFile.mkdirs();
+        }
+        if(!savePath.endsWith(File.separator)){
+            savePath = savePath+File.separator;
+        }
+        try{
+           OutputStream os = new FileOutputStream(savePath + fileName);
+            Process process = Runtime.getRuntime().exec("mysqldump -h " + ip + " -P "
+                    + port + " -u " + userName + " -p" + password + " --set-charset=UTF8 " + dataBaseName
+                    + " " + tableName);
+            InputStream in = process.getInputStream();
+            FileUtils.saveText(in,os);
+            if(process.waitFor() == 0){//0 表示线程正常终止。
+                return true;
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        if(dbBackup("127.0.0.1",3306,"root","introcks1234","D://backDB","test.sql","intellif_oms"))
+            System.out.println("备份成功");
+        else
+            System.out.println("备份失败");
+    }
+}
---
+++ src/main/java/com/intellif/utils/FileUtils.java
@@ -0,0 +1,44 @@
+package com.intellif.utils;
+
+import java.io.*;
+
+public class FileUtils {
+
+    /**
+     * 数据保存
+     * @param writer
+     * @param reader
+     */
+    public static void saveText(BufferedReader reader,PrintWriter writer){
+        try{
+            String line = null;
+            while((line = reader.readLine())!=null){
+                writer.println(line);
+            }
+            writer.flush();
+        }catch (Exception e){
+            throw new RuntimeException(e);
+        }finally {
+            if(writer!=null){
+                writer.close();
+            }
+            if(reader!=null){
+                try {
+                    reader.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    public static void saveText(InputStream in,OutputStream os){
+        try {
+            BufferedReader reader = new BufferedReader(new InputStreamReader(in,"utf-8"));
+            PrintWriter writer = new PrintWriter(new OutputStreamWriter(os,"utf-8"));
+            saveText(reader,writer);
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+    }
+}
